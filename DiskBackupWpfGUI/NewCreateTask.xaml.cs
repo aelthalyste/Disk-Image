d0d7f78efc3032afbeb94e7fe3e1d25c0eb3de58
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-09-23 12:46:16 +0300

    GUI: New Window Add

--- DiskBackupWpfGUI/NewCreateTask.xaml.cs
+++ DiskBackupWpfGUI/NewCreateTask.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DiskBackupWpfGUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewCreateTask.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewCreateTask : Window
     {
-        public MainWindow()
+        public NewCreateTask()
         {
             InitializeComponent();
-        }
-
-        private void btnCreateTask_Click(object sender, RoutedEventArgs e)
-        {
-            NewCreateTask newCreateTask = new NewCreateTask();
-            newCreateTask.ShowDialog();
         }
     }
 }
