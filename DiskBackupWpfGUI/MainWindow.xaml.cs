a2a770952c1c0bf4099bc39bc15818607bcc0d5d
Ebru VURAL <ebruvural29@gmail.com>
2020-09-30 13:53:49 +0300

    GUI: Grouping was added to disk listview

--- DiskBackupWpfGUI/MainWindow.xaml.cs
+++ DiskBackupWpfGUI/MainWindow.xaml.cs
@@ -20,10 +20,131 @@ namespace DiskBackupWpfGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool diskAllControl = false;
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Discs> discsItems = new List<Discs>();
+            discsItems.Add(new Discs() { 
+                checkSelect = false, 
+                Volume = "System Reserverd", 
+                Letter = 'A', 
+                Area = "2 GB", 
+                FreeSize = "1 GB", 
+                Type = "GPT", 
+                FileSystem = "NTFS", 
+                Statu = "Sağlıklı",
+                DiscName = "Disk 1"
+            });
+            discsItems.Add(new Discs()
+            {
+                checkSelect = false,
+                Volume = "Local Volume",
+                Letter = 'B',
+                Area = "75 GB",
+                FreeSize = "20 GB",
+                Type = "GPT",
+                FileSystem = "NTFS",
+                Statu = "Sağlıklı",
+                DiscName = "Disk 1"
+            });
+            discsItems.Add(new Discs()
+            {
+                checkSelect = false,
+                Volume = "System Reserverd",
+                Letter = 'C',
+                Area = "2 GB",
+                FreeSize = "1 GB",
+                Type = "GPT",
+                FileSystem = "NTFS",
+                Statu = "Sağlıklı",
+                DiscName = "Disk 2"
+            });
+            discsItems.Add(new Discs()
+            {
+                checkSelect = true,
+                Volume = "Local Volume",
+                Letter = 'D',
+                Area = "75 GB",
+                FreeSize = "20 GB",
+                Type = "GPT",
+                FileSystem = "NTFS",
+                Statu = "Sağlıklı",
+                DiscName = "Disk 2"
+            });
+
+            diskListBox.ItemsSource = discsItems;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(diskListBox.ItemsSource);
+            PropertyGroupDescription groupDescription = new PropertyGroupDescription("DiscName");
+            view.GroupDescriptions.Add(groupDescription);
         }
+
+        public class Discs
+        {
+            public bool checkSelect { get; set; }
+            public string Volume { get; set; }
+            public char Letter { get; set; }
+            public string Area { get; set; }
+            public string FreeSize { get; set; }
+            public string Type { get; set; }
+            public string FileSystem { get; set; }
+            public string Statu { get; set; }
+            public string DiscName { get; set; }
+        }
+
+        private void chbDiskSelectDiskAll_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (Discs item in diskListBox.ItemsSource)
+            {
+                item.checkSelect = true;
+                diskListBox.SelectedItems.Add(item);
+            }
+        }
+
+        private void chbDiskSelectDiskAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (diskAllControl == false)
+            {
+                foreach (Discs item in diskListBox.ItemsSource)
+                {
+                    item.checkSelect = false;
+                    diskListBox.SelectedItems.Remove(item);
+                }
+            }
+        }
+
+        private void chbDisk_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chbDiskSelectDiskAll.IsChecked == false)
+            {
+                bool diskControl = true;
+                foreach (Discs item in diskListBox.ItemsSource)
+                {
+                    MessageBox.Show(item.Letter.ToString() + " " + item.checkSelect.ToString());
+                    if (item.checkSelect == false)
+                    {
+                        diskControl = false;
+                        break;
+                    }
+                }
+                if (diskControl == true)
+                {
+                    chbDiskSelectDiskAll.IsChecked = true;
+                }
+            }
+        }
+
+        private void chbDisk_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chbDiskSelectDiskAll.IsChecked = false;
+            diskAllControl = true;
+        }
+
+        /*
+         *
+         */
 
         private void btnCreateTask_Click(object sender, RoutedEventArgs e)
         {
@@ -116,5 +237,6 @@ namespace DiskBackupWpfGUI
         {
             stackUpload.IsEnabled = false;
         }
+
     }
 }
