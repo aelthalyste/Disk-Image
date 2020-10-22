b398a881bfc656cd75987dd3e626d4e5043c7cc6
melik.yucel <melik.yucel@bilisimcenter.com>
2020-10-22 10:47:55 +0300

    Task pause ve resume işlemleri için PoC çalışması.

---
+++ WaitEventPoC/BackupManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitEventPoC
+{
+    class BackupManager
+    {
+        private ManualResetEvent _resetEvent = new ManualResetEvent(true);
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+        private bool _isStarted = false;
+        public void CreateBackup()
+        {
+            _isStarted = true;
+            var ct = _cts.Token;
+            Task.Run(() => {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        //cleanup 
+                        Console.WriteLine($"Job cancelled at %{i}");
+                        return;
+                    }
+                    _resetEvent.WaitOne();
+                    Thread.Sleep(500);
+                    Console.WriteLine($"%{i + 1} Completed");
+                    //do some backup thing
+                }
+
+            });
+        }
+
+        public void Pause()
+        {
+            if (!_isStarted) throw new Exception("Backup is not started");
+            _resetEvent.Reset();
+        }
+
+        public void Resume()
+        {
+            if (!_isStarted) throw new Exception("Backup is not started");
+            _resetEvent.Set();
+        }
+
+
+        public void CancelTask()
+        {
+            _cts.Cancel();
+            _isStarted = false;
+            _resetEvent.Set();
+        }
+
+
+    }
+}
---
+++ WaitEventPoC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WaitEventPoC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WaitEventPoC")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b0ac178-3fe5-495b-a9f8-14a466d10019")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
