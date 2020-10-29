af76323b4a513eee254ab8c090730ad5fcec4472
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-10-29 16:08:23 +0300

    GUI: taskScheduler db integration test start

---
+++ DiskBackup.TaskScheduler.Tests/SerializationTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quartz;
+using Quartz.Impl.Calendar;
+using Quartz.Logging;
+
+namespace DiskBackup.TaskScheduler.Tests
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        
+    }
+}
