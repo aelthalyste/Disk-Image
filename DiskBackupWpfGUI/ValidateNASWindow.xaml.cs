0d87d72aa37105c30b6466f90f9581e7fcb1e5f5
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-12-25 13:05:55 +0300

    GUI: ValidateNASWindow added

---
+++ DiskBackupWpfGUI/ValidateNASWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiskBackupWpfGUI
+{
+    /// <summary>
+    /// Interaction logic for ValidateNAS.xaml
+    /// </summary>
+    public partial class ValidateNASWindow : Window
+    {
+        public ValidateNASWindow()
+        {
+            InitializeComponent();
+        }
+        private void MyTitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnValidateNAS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnValidateNASClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
