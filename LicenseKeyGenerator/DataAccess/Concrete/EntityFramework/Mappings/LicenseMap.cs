5a713e2274e6af29e9cfe6ce73692311692fee31
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2021-01-29 13:02:33 +0300

    GUI: LicenseKey db operations add

---
+++ LicenseKeyGenerator/DataAccess/Concrete/EntityFramework/Mappings/LicenseMap.cs
@@ -0,0 +1,29 @@
+﻿using LicenseKeyGenerator.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseKeyGenerator.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class LicenseMap : EntityTypeConfiguration<License>
+    {
+        public LicenseMap()
+        {
+            ToTable(@"license_info");
+            HasKey(x => x.UniqKey);
+
+            Property(x => x.UniqKey).HasColumnName("uniq_key");
+            Property(x => x.DealerName).HasColumnName("dealer_name");
+            Property(x => x.CustomerName).HasColumnName("customer_name");
+            Property(x => x.AuthorizedPerson).HasColumnName("authorized_person");
+            Property(x => x.SupportEndDate).HasColumnName("support_end_date");
+            Property(x => x.CreatedDate).HasColumnName("created_date");
+            Property(x => x.LicenseVersion).HasColumnName("license_version");
+            Property(x => x.Key).HasColumnName("key");
+        }
+
+    }
+}
---
+++ LicenseKeyGenerator/Entities/License.cs
@@ -0,0 +1,27 @@
+﻿using LicenseKeyGenerator.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseKeyGenerator.Entities
+{
+    public class License: IEntity
+    {
+        public string UniqKey { get; set; }
+        public string DealerName { get; set; }
+        public string CustomerName { get; set; }
+        public string AuthorizedPerson { get; set; }
+        public DateTime SupportEndDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public VersionType LicenseVersion { get; set; }
+        public string Key { get; set; }
+    }
+    public enum VersionType
+    {
+        Server = 0,
+        Workstation = 1,
+        SBS = 2
+    }
+}
