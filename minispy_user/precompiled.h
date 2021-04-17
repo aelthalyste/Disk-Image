9cc012d61e00e7035daf8967023336e0da428994
Aelthalyste <guner.batuhan@gmail.com>
2021-04-17 04:49:32 +0300

    Precompiled headers, 50x build speedup! Small name changes around codebase.

---
+++ minispy_user/precompiled.h
@@ -0,0 +1,37 @@
+/* date = April 16th 2021 4:34 pm */
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <stdint.h>
+#include <vector>
+#include <winioctl.h>
+#include <emmintrin.h>
+
+#include <string>
+#include <vector>
+#include <atlbase.h>
+
+#include <vds.h>
+#include <vss.h>
+#include <vswriter.h>
+#include <vsbackup.h>
+#include <vsmgmt.h>
+
+
+#include<iostream>
+
+#include <fltUser.h>
+#include <intrin.h>
+
+#include <functional>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+
+#include "iphlpapi.h"
+
+
+#include "nar.h"
+#include "../inc/minispy.h"
