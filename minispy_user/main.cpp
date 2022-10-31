9d93845dfe781aa28b9ccd7d02839316d89e6d20
Batuhan <batuhan.guner@bilisimcenter.com>
2022-10-31 10:08:03 +0300

    Added webserver poc. Deleted old codes.

--- minispy_user/main.cpp
+++ minispy_user/main.cpp
@@ -263,20 +263,7 @@ void DoRestore(const char *Directory, char OutputVolumeLetter) {
 
 int main(int argc, char *argv[]) {
 
-    char Letter = 'C';
-    HANDLE vh = NarOpenVolume('C');
 
-    extension_finder_memory Memory = NarSetupExtensionFinderMemory(vh);
-    wchar_t *ext[] = {
-        L".dll",
-        // L".exe"
-    };
-    extension_search_result Result = NarFindExtensions(Letter, vh, ext, sizeof(ext) / (sizeof(ext[0])), &Memory);
-    LOG_INFO("Found %d files : ", Result.Len);
-
-    // for (uint64_t i = 0; i < Result.Len; ++i) {
-    //     LOG_INFO("-> %S", Result.Files[i]);
-    // }
     return 0;
 
     if      (0 == strcmp(argv[1], "list"))    ListChains(argv[2]);
@@ -286,109 +273,24 @@ int main(int argc, char *argv[]) {
 
 
 #if 0
-    backup_package Output[1024];
-    if (0) {
-        int c = NarGetBackupsInDirectory("D:\\", Output, 1024);
-        for(int i=0;i<c;++i) {
-            fprintf(stdout, "Volume  %c\n", Output[i].BackupInformation.Letter);
-            fprintf(stdout, "version %2d\n",  Output[i].BackupInformation.Version);
-            fprintf(stdout, "Size of binary data      %10llu (gb -> %10llu)\n", Output[i].BackupInformation.SizeOfBinaryData, Output[i].BackupInformation.SizeOfBinaryData/(1024ull * 1024 * 1024));
-            fprintf(stdout, "Original size of volume  %10llu (gb -> %10llu)\n", Output[i].BackupInformation.OriginalSizeOfVolume, Output[i].BackupInformation.SizeOfBinaryData / (1024ull * 1024 * 1024));
-            fprintf(stdout, "########\n");
-        }
-    }
+    char Letter = 'C';
+    HANDLE vh = NarOpenVolume('C');
 
-    if (0) {
-        char Dir[] = "D:\\";
-        auto arr = NarGetChainsInDirectory(Dir);
-        NarFreeChains(arr);
-
-        nar_binary_files *arr2 = NarGetBinaryFilesInDirectory(Dir, arr[0][0].BackupID, 4);
-        NarFreeBinaryFilesInDirectory(arr2);        
-    }
-
-    size_t bsize = 64 * 1024 * 1024;
-    void *MemBuf = malloc(bsize);
-    
-    LOG_CONTEXT C = {0};
-    C.Port = INVALID_HANDLE_VALUE;
-    
-    if(SetupVSS() && ConnectDriver(&C)){
-        char Volume = 0;
-        int Type = 1;
-        
-        DotNetStreamInf inf = {0};
-        printf("ENTER LETTER TO DO BACKUP\n");
-        std::cin>>Volume;
-        
-        uint64_t BFCAP = Megabyte(16);
-        void *bf = malloc(BFCAP);
-
-        for (int __i=0;
-            __i<5;
-            ++__i) 
-        {
-            
-            memset(&inf, 0, sizeof(inf));
-            BackupType bt = BackupType::Inc;
-
-            uint64_t TotalRead    = 0;
-            uint64_t TotalWritten = 0;
-            uint64_t BytesToTransfer = 0;
-
-            char BinaryExtension[256];
-            memset(BinaryExtension,0, sizeof(BinaryExtension));
-
-            if(SetupStream(&C, (wchar_t)Volume, bt, &BytesToTransfer, BinaryExtension, NAR_COMPRESSION_NONE)){
-                
-                int id = GetVolumeID(&C, (wchar_t)Volume);
-                volume_backup_inf *v = &C.Volumes[id];
-
-                char fn[1024];
-                fn[0] = 0;
-                snprintf(fn, sizeof(fn), "D:\\tempfile-%d.%s", __i, BinaryExtension);
-
-
-                File binary_file = create_file(fn);
-                BG_ASSERT(is_file_handle_valid(&binary_file));
-
-                v->Stream.RecIndex = v->Stream.RecordCount;
-                
-                {
-                    bool r = write_file(&binary_file, bf, BFCAP);
-                    BG_ASSERT(r);
-                }
-
-
-                {
-                    uint32_t btw = ReadStream(&v->Stream, bf, BFCAP);
-                    BG_ASSERT(btw);
-
-                    bool r = write_file(&binary_file, bf, btw);
-                    BG_ASSERT(r);
-                }
-
-                char MetadataOutput[1024];
-                if (TerminateBackup(v, 1, "D:\\", MetadataOutput)) {
-                    printf("Metadata saved as %s", MetadataOutput);
-                }
-                else {
-                    BG_ASSERT(false);
-                    printf("Unable to terminate backup!\n");
-                }
-
-                close_file(&binary_file);
-            }
-        }
-
-    }
-    else {
-
-    }
-
-    return 0;
+    extension_finder_memory Memory = NarSetupExtensionFinderMemory(vh);
+    wchar_t* ext[] = {
+        L".dll",
+        // L".exe"
+    };
+    extension_search_result Result = NarFindExtensions(Letter, vh, ext, sizeof(ext) / (sizeof(ext[0])), &Memory);
+    LOG_INFO("Found %d files : ", Result.Len);
+    // for (uint64_t i = 0; i < Result.Len; ++i) {
+    //     LOG_INFO("-> %S", Result.Files[i]);
+    // }
 #endif
+
 }
+
+
 
 
 
@@ -448,78 +350,6 @@ wmain(int argc, wchar_t* argv[]) {
     }
 #endif
     
-#if 0
-    
-    nar_arena Arena = ArenaInit(malloc(1024*1024*512), 1024*1024*512);
-    
-    NarUTF8 CompletePath = NARUTF8("testfile.txt");
-    NarUTF8 Root2 = NarGetRootPath(CompletePath, &Arena);
-    
-    
-    file_explorer FE = NarInitFileExplorer(NARUTF8("G:\\NB_M_2-C07291017.nbfsm"));
-    
-    file_explorer_file *Target = FEFindFileWithID(&FE, 76831);
-    
-    //printf("%S\n", FEGetFileFullPath(&FE, Target));
-    file_explorer_file *R = FEStartParentSearch(&FE, 5);
-    
-    nar_backup_id ID = {};
-    ID.Q        = 18877558573959141;
-    int Version = 2;
-    
-    
-    NarUTF8 Root;
-    Root.Str = (uint8_t*)"G:\\";
-    Root.Len = 3;
-    Root.Cap = 0;
-    
-    file_restore_ctx Ctx= NarInitFileRestoreCtx(&FE, Target, &Arena);
-    size_t FCMSize = Megabyte(2);
-    void* FCM = ArenaAllocateAligned(&Arena, FCMSize, 8);
-    
-    HANDLE File = CreateFileA("output", GENERIC_WRITE | GENERIC_READ, 0, 0, CREATE_ALWAYS, 0, 0);
-    ASSERT(File != INVALID_HANDLE_VALUE);
-    for(;;){
-        auto Result = NarAdvanceFileRestore(&Ctx, FCM, FCMSize);
-        if(Result.Len == 0){
-            break;
-        }
-        DWORD R = SetFilePointer(File, Result.Offset, 0, FILE_BEGIN);
-        DWORD BytesWritten = 0;
-        
-        if(!WriteFile(File, FCM, Result.Len, &BytesWritten, 0) || BytesWritten!=Result.Len){
-            ASSERT(FALSE);
-            printf("Error code %X\n", GetLastError());
-        }
-        else{
-            printf("%8u\t%8u  MAIN\n", Result.Offset/4096, Result.Len/4096);
-        }
-        
-    }
-    
-    CloseHandle(File);
-    NAR_BREAK;
-    return 0;
-    
-#if 0    
-    while(1){
-        uint32_t FileFound = 0;
-        for(file_explorer_file *File = FEStartParentSearch(&FE, UserGivenID);
-            File != 0;
-            File = FENextFileInDir(&FE, File)){
-            printf("%4u %.20S %I64u\n", File->FileID, File->Name, File->Size);
-            FileFound++;
-        }
-        
-        printf("\nFound %u files\n", FileFound);
-        
-        scanf("%u \n", &UserGivenID);
-        printf("\n\n#############\n\n");
-    }
-#endif
-    
-    
-#endif
     
     
 #if 0    
@@ -617,130 +447,7 @@ wmain(int argc, wchar_t* argv[]) {
 #endif
     
     
-    
-    size_t bsize = 64 * 1024 * 1024;
-    void *MemBuf = malloc(bsize);
-    
-    LOG_CONTEXT C = {0};
-    C.Port = INVALID_HANDLE_VALUE;
-    
-    if(SetupVSS() && ConnectDriver(&C)){
-        DotNetStreamInf inf = {0};
-        
-        char Volume = 0;
-        int Type = 1;
-        loop{
-            
-            memset(&inf, 0, sizeof(inf));
-            
-            printf("ENTER LETTER TO DO BACKUP\n");
-            std::cin>>Volume;
-            
-            BackupType bt = BackupType::Inc;
-            
-            if(SetupStream(&C, (wchar_t)Volume, bt, &inf, false)){
-                
-                int id = GetVolumeID(&C, (wchar_t)Volume);
-                volume_backup_inf *v = &C.Volumes.Data[id];
-                size_t TotalRead    = 0;
-                size_t TotalWritten = 0;
-                size_t TargetWrite  = (size_t)inf.ClusterSize * (size_t)inf.ClusterCount;
-                
-                printf("filename : %S\n", inf.FileName.c_str());
-                
-                HANDLE file = CreateFileW(inf.FileName.c_str(), 
-                                          GENERIC_WRITE | GENERIC_READ, 
-                                          0, 0, 
-                                          CREATE_ALWAYS, 
-                                          0, 0);
-                if(file != INVALID_HANDLE_VALUE){
-                    uint64_t ucs = 0;
-                    printf("starting backup\n");
-#if 1                    
-                    loop{
-                        int Read = ReadStream(&v->Stream, MemBuf, (uint32_t)bsize);
-                        TotalRead += Read;
-                        
-                        ucs += v->Stream.BytesProcessed;
-                        
-                        if(Read == 0){
-                            break;
-                        }
-                        else{
-                            DWORD BytesWritten = 0;
-                            if(WriteFile(file, MemBuf, Read, &BytesWritten, 0) && BytesWritten == Read){
-                                TotalWritten += BytesWritten;
-                            }
-                            else{
-                                NAR_BREAK;
-                                printf("Couldnt write to file\n");
-                            }
-                            ASSERT(BytesWritten == Read);
-                        }
-                    }
-#else
-                    
-                    TotalRead = TargetWrite;
-                    TotalWritten = TargetWrite;
-#endif
-                    
-                    
-                    if(CheckStreamCompletedSuccessfully(v)){
-                        TerminateBackup(v, NAR_SUCC, L"D:\\");
-                    }
-                    else{
-                        NAR_BREAK;
-                        TerminateBackup(v, NAR_FAILED, L"D:\\");
-                    }
-                    
-                    NarSaveBootState(&C);
-                }
-                else{
-                    // NOTE(Batuhan): couldnt create file to save backup
-                    NAR_BREAK;
-                    int ret = TerminateBackup(v, NAR_FAILED, 0);
-                    ret++;// to inspect ret in debugging
-                }
-                
-                CloseHandle(file);
-                
-            }
-            else{
-                NAR_BREAK;
-                printf("couldnt setup stream\n");
-            }
-        }
     }
-    else{
-        NAR_BREAK;
-    }
-    
-    return 0;
-    
-    UNREFERENCED_PARAMETER(argc);
-    UNREFERENCED_PARAMETER(argv);
-    return 0;
-}
 
 #endif
 
-
-
-
-//debug_record GlobalDebugRecordArray[__COUNTER__];
-
-inline void
-PrintDebugRecords() {
-    
-#if 0    
-    int len = sizeof(GlobalDebugRecordArray)/sizeof(GlobalDebugRecordArray[0]);
-    for (int i = 0; i < len; i++) {
-        if(GlobalDebugRecordArray[i].FunctionName == 0){
-            continue;
-        }
-        printf("FunctionName: %s\tDescription: %s\tAVGclocks: %.3f\tat line %i\tHit count %i\n", GlobalDebugRecordArray[i].FunctionName, GlobalDebugRecordArray[i].Description, (double)GlobalDebugRecordArray[i].Clocks / GlobalDebugRecordArray[i].HitCount, GlobalDebugRecordArray[i].LineNumber, GlobalDebugRecordArray[i].HitCount);
-        
-    }
-#endif
-    
-}
---
+++ minispy_user/src/webserver_poc.cpp
@@ -0,0 +1,139 @@
+﻿#define NO_SSL
+#include "civetweb.h"
+
+
+#include <assert.h>
+#include <Windows.h>
+#include <string.h>
+#include <stdio.h>
+
+struct user_data {
+	int a;
+	int b;
+};
+
+constexpr const char *HTTP_POST = "POST";
+constexpr const char* HTTP_GET  = "GET";
+
+int hello_handler(mg_connection* conn, void* cbdata) {
+
+	const mg_request_info *inf = mg_get_request_info(conn);
+	fprintf(stdout, "uri : %s\n", inf->request_uri);
+	if (strcmp(inf->request_method, "GET") == 0) {
+		mg_send_http_error(conn, 403, "some error about stuff");
+		char msg[] = "this is an http ok response!";
+		
+		//mg_send_http_ok(conn, "application/octet-stream", 100);
+		//mg_printf(conn, "myheader2 : somevalu66e\r\n");
+		//mg_write(conn, msg, sizeof(msg));
+
+		char dst[256] = {};
+		char query_to_search[] = "some_squery";
+		int ret = 0;
+		if (inf->query_string) {
+			ret = mg_get_var(inf->query_string, strlen(inf->query_string), query_to_search, dst, sizeof(dst));
+		}
+		fprintf(stdout, "searched for query : %s, found value : %s, ret : %d\n", query_to_search, dst, ret);
+		// mg_printf(conn,
+		// 	"HTTP/1.1 405 Method Not Allowed\r\nConnection: close\r\n");
+		// mg_printf(conn, "Content-Type: text/plain\r\n\r\n");
+		// mg_printf(conn, "HTTP/1.1 405 Method Not Allowed\r\n");
+		// mg_printf(conn, "Content-Type: text/plain\r\n\r\n");
+		
+		// mg_response_header_add(conn, "CustomHeader2", "text/something22?", -1);
+	}
+	
+	
+	return 200;
+}
+
+#define LOG_FUNCTION fprintf(stdout, "%s\n", __FUNCTION__);
+
+int hello_subpage_handler(mg_connection* conn, void* cbdata) {
+	LOG_FUNCTION;
+	const mg_request_info* inf = mg_get_request_info(conn);
+	fprintf(stdout, "uri : %s\n", inf->request_uri);
+	return 200;
+}
+
+
+
+int begin_request(struct mg_connection*) {
+	LOG_FUNCTION;
+	return 0;
+}
+
+void end_request(const struct mg_connection*, int reply_status_code) {
+	LOG_FUNCTION;
+	return;
+}
+
+int init_connection(const struct mg_connection* conn, void** conn_data) {
+	LOG_FUNCTION;
+	return 0;
+}
+
+void connection_close(const struct mg_connection* ctx) {
+	LOG_FUNCTION;
+}
+
+void connection_closed(const struct mg_connection* ctx) {
+	auto d = (user_data*)mg_get_user_context_data(ctx);
+	LOG_FUNCTION;
+}
+
+void* init_thread(const struct mg_context* ctx, int thread_type) {
+	// mg_set_user_connection_data
+	// int index = TlsAlloc();	
+	// TlsSetValue(index, );
+	 
+	LOG_FUNCTION;
+	return NULL;
+}
+
+void exit_thread(const struct mg_context* ctx, int thread_type, void* thread_pointer) {
+	LOG_FUNCTION;
+}
+
+int main() {
+	
+	user_data data;
+	data.a = 25;
+	data.b = 62;
+
+	mg_context* ctx = NULL;
+
+	mg_init_library(0);
+	
+	mg_callbacks callbacks = {};
+	callbacks.begin_request = begin_request;
+	callbacks.end_request = end_request;
+	callbacks.init_connection = init_connection;
+	callbacks.connection_close = connection_close;
+	callbacks.connection_closed = connection_closed;
+	callbacks.init_thread = init_thread;
+	callbacks.exit_thread = exit_thread;
+
+	mg_option a;
+	
+	const char* options[] = {
+	  "listening_ports", "5003",
+	  NULL
+	};
+
+	ctx = mg_start(&callbacks, &data, options);
+	assert(ctx);
+
+	mg_set_request_handler(ctx, "/hello/subpage", hello_subpage_handler, NULL);
+	mg_set_request_handler(ctx, "/hello", hello_handler, NULL);
+
+	
+	while (1) {
+		Sleep(50);
+	}
+
+	mg_stop(ctx);
+	mg_exit_library();
+
+	return 0;
+}
